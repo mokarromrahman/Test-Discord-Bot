24d65900d4c498a64add9eac4a4e7d86f5c2b972
Mokarrom Rahman <mrahman0000@gmail.com>
2020-07-13 16:20:50 -0600

    finished the first tutorial

---
+++ DiscordBotTutorial/DiscordBotTutorial/Bot.cs
@@ -0,0 +1,77 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DiscordBotTutorial
+{
+    public class Bot
+    {
+        //Current discord client being used
+        public DiscordClient DiscordClient { get; private set; }
+
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            //hold the json variables
+            string json = "";
+
+            //open and read the config.json file
+            using (FileStream fs = File.OpenRead("config.json"))
+            {
+                using(StreamReader sr = new StreamReader(fs, new UTF8Encoding(false)))
+                {
+                    //false makes it so that the same thread does not
+                    //have to continue the task
+                    json = await sr.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
+
+            //conver the string into this object
+            ConfigJSON configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            //instantiate with the configs we need
+            DiscordConfiguration discordConfiguration = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            //create new client with the configs
+            DiscordClient = new DiscordClient(discordConfiguration);
+
+            DiscordClient.Ready += OnClientReady;
+
+
+            //Set the commands
+            CommandsNextConfiguration cnc = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { configJson.CommandPrefix },
+                EnableDms = false,
+                //Allows you to say @Bot command
+                EnableMentionPrefix = true
+            };
+
+            Commands = DiscordClient.UseCommandsNext(cnc);
+
+            await DiscordClient.ConnectAsync();
+
+            //Set a delay so that the bot does not quit
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask; 
+        }
+    }
+}
---
+++ DiscordBotTutorial/DiscordBotTutorial/ConfigJSON.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBotTutorial
+{
+    public struct ConfigJSON
+    {
+        //this initializes the token for us
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        //this initializes the prefix for us
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
